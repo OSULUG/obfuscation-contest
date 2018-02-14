66a0bbdb5579974daf7316899b4a4c444bb1f3ea
Mars Bomber <bomberm@oregonstate.edu>
2018-02-13 18:48:04 -0800

    Lets see how you like this

---
+++ MariesSubmission/defs.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#define fish std::string
+#define solong ;
+#define IF std::cout
+#define towel std::endl
+#define andthanks return
+#define forall 0
+#define thefish ;}
---
+++ MariesSubmission/mariessubmission.cpp
@@ -0,0 +1,2 @@
+#include "defs.h"
+int main(void){fish 🌍  ="world" solong fish 👋=  "hello " solong IF << 👋 + 🌍 << towel solong andthanks forall thefish
