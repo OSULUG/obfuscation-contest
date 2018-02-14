4c803be9c8d432e6f452069d4d89a48df9e6d350
Jonathan Frederick <doublej472@gmail.com>
2018-02-13 18:49:05 -0800

    Add my code

---
+++ examples/doublej472.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define a int
+#define b main
+#define c long
+#define d putchar
+#define g return
+#define h while
+#define i char*
+a b(){c f[
+]={0x77202c6f6c6c6548
+,0x00000a21646c726f};i
+e=((i)f);h
+(*e)d(*e++
+);g
+e[0];}
