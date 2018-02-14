74abf4e26a1a09fa016defee9b00deeba540c1c4
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-02-13 16:21:40 -0800

    add some obfuscation examples

---
+++ examples/arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	char* string = "abcdefg";
+	printf("%c%c%c\n",
+
+		string[2],
+
+		0[string],
+
+		13 + (0[string] - 90)[string-1]);
+
+	return 0;
+}
---
+++ examples/cpp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define C printf(
+#define IS "Hello, world!"
+#define AWESOME );
+
+int main() {
+	C IS AWESOME
+}
