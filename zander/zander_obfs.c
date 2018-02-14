8bccff94a55fe79aa7b7b73b235ba7e6e3891892
Alexander Nead-Work <neadwora@oregonstate.edu>
2018-02-13 18:54:40 -0800

    obfuscated

---
+++ zander/zander_obfs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#define NO "hheelllloowwoorrlldd"
+int main(){int i=0;for(;i<=10;i++){if(i==5){printf("%c\
+",(int)0x20);}else{printf("%c",ggnore(i));}}printf("\n\
+");return 0;}int ggnore(int pos){if((!0&&1||0||(0&&1)||(0||1))&&pos>=5){
+pos--;}if (strcmp("111112222111444444","thisisnotthedroid\
+yourelookingfor")>0&&pos==2){pos=3;}if((1&&0||0||1&&0||(1&&!0))&&pos==3){
+pos=8;}if((NO[pos]||pos[NO])&&!(strcmp("(1&&0||0||1&&0||(1&&!0))","(!0&&1||0\
+||(0&&1)||(0||1)")<0)&&pos==8){pos=2;}if((NO[pos]||pos[NO])&&pos==4){pos = 6;
+}if((NO[pos]||pos[NO])&&pos==6){pos=4;}if (NO[pos] || pos[NO]) {
+return NO[((pos[NO]==NO[pos])+pos-1)*2];}}
